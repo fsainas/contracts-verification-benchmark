c92fefd3b4d9b0695e4de2946892a52cc5421446
Franco Sainas <franco@sainas.me>
2023-03-17 12:11:37 +0100

    cleaned Escrow.sol

--- algoml/Escrow.sol
+++ algoml/Escrow.sol
@@ -62,7 +62,7 @@ contract Escrow {
         seller = _seller;
         deposit = msg.value;
 
-        //assert(buyer != seller);        // Proven
+        assert(buyer != seller);        // Proven
     }
 
     /*****************
@@ -115,15 +115,15 @@ contract Escrow {
         require(block.number > end_redeem);
         
         require(fee_rate <= 10000);     // The fee cannot be more then the deposit
-        //assert(fee_rate / 10000 <= 1);  // Proven
+        assert(fee_rate / 10000 <= 1);  // Proven
         uint fee = deposit * (fee_rate / 10000);
-        //assert(deposit >= fee);         // Proven
-        deposit = deposit - fee;
-        //assert(deposit >= fee);         // CHC: Assertion violation (fee_rate > 50%)
-        //(bool success,) = escrow.call{value: fee}("");
-        //if (!success) deposit += fee;
+        assert(deposit >= fee);         // Proven
+        deposit -= fee;
+        assert(deposit >= fee);         // CHC: Assertion violation (fee_rate > 50%)
+        (bool success,) = escrow.call{value: fee}("");
+        if (!success) deposit += fee;
     
-        //require(success);
+        require(success);
         eChoice = _eChoice;
     }
 
@@ -136,13 +136,7 @@ contract Escrow {
         (bool success,) = eChoice.call{value: amount}("");
         if (!success) deposit += amount;
         require(success);
-        //assert(eChoice == address(0) || eChoice == buyer_choice || eChoice == seller_choice);       // this is interensting
     }
 
-    function invariant() public view {
-        //assert(eChoice == address(0) || eChoice == buyer_choice || eChoice == seller_choice);       // this is interensting
-        //assert(eChoice != address(0) && block.number < end_redeem);     // this gives a counterexample
-        //assert(!(block.number < end_redeem && !(eChoice == address(0)))); //
-        //assert(deposit >= 0);           // Proven, deposit is a uint so it's better to check if deposit can underflow
-    }
+    function invariant() public view { }
 }
