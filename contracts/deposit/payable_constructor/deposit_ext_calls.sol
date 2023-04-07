57730a71936ac960f288f9a1b41f108de383bc62
Franco Sainas <franco@sainas.me>
2023-04-07 12:32:20 +0200

    deposit payable constructor

---
+++ contracts/deposit/payable_constructor/deposit_ext_calls.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity >= 0.8.2;
+
+contract Deposit {
+
+    uint balance;
+    uint sent;
+
+    uint deposited;
+
+    constructor () payable {
+        balance = msg.value;
+        deposited = balance;
+    }
+
+    function deposit(uint _amount) public {
+        balance += _amount;
+        deposited += _amount;
+    }
+
+    function withdraw(uint _amount) public {
+
+        require(_amount <= balance);
+
+        sent += _amount;
+        balance -= _amount;
+
+        (bool succ,) = msg.sender.call{value: _amount}("");
+        require(succ);
+    }
+
+    function withdraw_all() public {
+
+        uint amount = balance;
+
+        sent += balance;
+        balance = 0;
+
+        (bool succ,) = msg.sender.call{value: amount}("");
+        require(succ);
+    }
+
+    function invariant() public view {
+        assert(sent <= deposited);
+    }
+}
+// ====
+// SMTEngine: CHC
+// Time: 15.79s
+// Targets: "all"
+// ----
+// Warning: CHC: Overflow line 17
+// Warning: CHC: Overflow line 18
