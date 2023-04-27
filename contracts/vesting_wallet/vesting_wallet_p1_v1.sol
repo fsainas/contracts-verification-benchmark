cbd42c2c33e0263a5225047dfcaee51e0022f49d
Franco Sainas <franco@sainas.me>
2023-04-27 17:26:18 +0200

    vesting wallet: clean p1 v1

--- contracts/vesting_wallet/vesting_wallet_p1_v1.sol
+++ contracts/vesting_wallet/vesting_wallet_p1_v1.sol
@@ -5,16 +5,6 @@ pragma solidity ^0.8.0;
 import "lib/Address.sol";
 import "lib/Context.sol";
 
-/**
- * @title VestingWallet
- * @dev This contract handles the vesting of Eth and ERC20 tokens for a given beneficiary. Custody of multiple tokens
- * can be given to this contract, which will release the token to the beneficiary following a given vesting schedule.
- * The vesting schedule is customizable through the {vestedAmount} function.
- *
- * Any token transferred to this contract will follow the vesting schedule as if they were locked from the beginning.
- * Consequently, if the vesting has already started, any amount of tokens sent to this contract will (at least partly)
- * be immediately releasable.
- */
 contract VestingWallet is Context {
     event EtherReleased(uint256 amount);
 
@@ -23,9 +13,6 @@ contract VestingWallet is Context {
     uint64 private immutable _start;
     uint64 private immutable _duration;
 
-    /**
-     * @dev Set the beneficiary, start timestamp and vesting duration of the vesting wallet.
-     */
     constructor(address beneficiaryAddress, uint64 startTimestamp, uint64 durationSeconds) payable {
         require(beneficiaryAddress != address(0), "VestingWallet: beneficiary is zero address");
         _beneficiary = beneficiaryAddress;
@@ -33,69 +20,39 @@ contract VestingWallet is Context {
         _duration = durationSeconds;
     }
 
-    /**
-     * @dev The contract should be able to receive Eth.
-     */
     receive() external payable virtual {}
 
-    /**
-     * @dev Getter for the beneficiary address.
-     */
     function beneficiary() public view virtual returns (address) {
         return _beneficiary;
     }
 
-    /**
-     * @dev Getter for the start timestamp.
-     */
     function start() public view virtual returns (uint256) {
         return _start;
     }
 
-    /**
-     * @dev Getter for the vesting duration.
-     */
     function duration() public view virtual returns (uint256) {
         return _duration;
     }
 
-    /**
-     * @dev Amount of eth already released
-     */
-    function released() public view virtual returns (uint256) {
+   function released() public view virtual returns (uint256) {
         return _released;
     }
 
-    /**
-     * @dev Getter for the amount of releasable eth.
-     */
     function releasable() public view virtual returns (uint256) {
         return vestedAmount(uint64(block.timestamp)) - released();
     }
 
-    /**
-     * @dev Release the native token (ether) that have already vested.
-     *
-     * Emits a {EtherReleased} event.
-     */
-    function release() public virtual {
+   function release() public virtual {
         uint256 amount = releasable();
         _released += amount;
         emit EtherReleased(amount);
         Address.sendValue(payable(beneficiary()), amount);
     }
 
-    /**
-     * @dev Calculates the amount of ether that has already vested. Default implementation is a linear vesting curve.
-     */
     function vestedAmount(uint64 timestamp) public view virtual returns (uint256) {
         return _vestingSchedule(address(this).balance + released(), timestamp);
     }
 
-    /**
-     * @dev Virtual implementation of the vesting formula. This returns the amount vested, as a function of time, for
-     * an asset given its total historical allocation.
-     */
     function _vestingSchedule(uint256 totalAllocation, uint64 timestamp) internal view virtual returns (uint256) {
         if (timestamp < start()) {
             return 0;
