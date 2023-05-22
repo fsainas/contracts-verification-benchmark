100c10490b6f8dd0dd755f8523a0b35307bb165f
Franco Sainas <franco@sainas.me>
2023-05-22 17:03:20 +0200

    payment splitter: conformant to style rules

--- contracts/payment_splitter/payment_splitter.sol
+++ contracts/payment_splitter/payment_splitter.sol
@@ -5,83 +5,83 @@ pragma solidity ^0.8.0;
 
 contract PaymentSplitter {
 
-    uint256 private _totalShares;
-    uint256 private _totalReleased;
+    uint256 totalShares;
+    uint256 totalReleased;
 
-    mapping(address => uint256) private _shares;
-    mapping(address => uint256) private _released;
-    address[] private _payees;
+    mapping(address => uint256) private shares;
+    mapping(address => uint256) private released;
+    address[] private payees;
 
-    constructor(address[] memory payees, uint256[] memory shares_) payable {
-        require(payees.length == shares_.length, "PaymentSplitter: payees and shares length mismatch");
-        require(payees.length > 0, "PaymentSplitter: no payees");
+    constructor(address[] memory payees_, uint256[] memory shares_) payable {
+        require(payees_.length == shares_.length, "PaymentSplitter: payees and shares length mismatch");
+        require(payees_.length > 0, "PaymentSplitter: no payees");
 
         for (uint256 i = 0; i < payees.length; i++) {
-            _addPayee(payees[i], shares_[i]);
+            addPayee(payees_[i], shares_[i]);
         }
     }
 
     receive() external payable virtual { }
 
-    function totalShares() public view returns (uint256) {
-        return _totalShares;
+    function getTotalgetShares() public view returns (uint256) {
+        return totalShares;
     }
 
-    function totalReleased() public view returns (uint256) {
-        return _totalReleased;
+    function getTotalReleased() public view returns (uint256) {
+        return totalReleased;
     }
 
-    function shares(address account) public view returns (uint256) {
-        return _shares[account];
+    function getShares(address account) public view returns (uint256) {
+        return shares[account];
     }
 
-    function released(address account) public view returns (uint256) {
-        return _released[account];
+    function getReleased(address account) public view returns (uint256) {
+        return released[account];
     }
 
-    function payee(uint256 index) public view returns (address) {
-        return _payees[index];
+    function getPayee(uint256 index) public view returns (address) {
+        return payees[index];
     }
 
     function releasable(address account) public view returns (uint256) {
-        uint256 totalReceived = address(this).balance + totalReleased();
-        return _pendingPayment(account, totalReceived, released(account));
+        uint256 totalReceived = address(this).balance + getTotalReleased();
+        return pendingPayment(account, totalReceived, getReleased(account));
     }
 
     function release(address payable account) public virtual {
-        require(_shares[account] > 0, "PaymentSplitter: account has no shares");
+        require(shares[account] > 0, "PaymentSplitter: account has no shares");
 
         uint256 payment = releasable(account);
 
         require(payment != 0, "PaymentSplitter: account is not due payment");
 
-        // _totalReleased is the sum of all values in _released.
-        // If "_totalReleased += payment" does not overflow, then "_released[account] += payment" cannot overflow.
-        _totalReleased += payment;
+        // totalReleased is the sum of all values in released.
+        // If "totalReleased += payment" does not overflow, then "released[account] += payment" cannot overflow.
+        totalReleased += payment;
         unchecked {
-            _released[account] += payment;
+            released[account] += payment;
         }
 
         (bool success,) = account.call{value: payment}("");
         require(success);
     }
 
-    function _pendingPayment(
+    function pendingPayment(
         address account,
         uint256 totalReceived,
         uint256 alreadyReleased
     ) private view returns (uint256) {
-        return (totalReceived * _shares[account]) / _totalShares - alreadyReleased;
+        return (totalReceived * shares[account]) / totalShares - alreadyReleased;
     }
 
-    function _addPayee(address account, uint256 shares_) private {
+    function addPayee(address account, uint256 shares_) private {
         require(account != address(0), "PaymentSplitter: account is the zero address");
         require(shares_ > 0, "PaymentSplitter: shares are 0");
-        require(_shares[account] == 0, "PaymentSplitter: account already has shares");
+        require(shares[account] == 0, "PaymentSplitter: account already has shares");
 
-        _payees.push(account);
-        _shares[account] = shares_;
-        _totalShares = _totalShares + shares_;
+        payees.push(account);
+        shares[account] = shares_;
+        totalShares = totalShares + shares_;
     }
 
 }
