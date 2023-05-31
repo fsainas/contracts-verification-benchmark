db8d1f88c7a610290cfaef4f23dc7f2344fceaa6
Franco Sainas <franco@sainas.me>
2023-05-31 16:54:21 +0200

    added a new contract: Lottery

---
+++ contracts/Lottery/Lottery_v1.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.18;
+
+contract Lottery {
+    address immutable private manager;
+    address[] private players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() external payable {
+        require(msg.value == .01 ether);
+        
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encode(block.prevrandao)));
+    }
+
+    function pickWinner() public {
+        require(msg.sender == manager);
+
+        address winner = players[random() % players.length];
+        players = new address[](0);
+
+        (bool success,) = winner.call{value: address(this).balance}("");
+        require(success);
+    }
+
+}
---
+++ contracts/Lottery/Lottery_v2.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.18;
+
+contract Lottery {
+    address immutable private manager;
+    address[] private players;
+    mapping (address => bool) hasEntered;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() external payable {
+        require(msg.value == .01 ether);
+        require(!hasEntered[msg.sender]);
+        
+        hasEntered[msg.sender] = true;
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encode(block.prevrandao)));
+    }
+
+    function pickWinner() public {
+        require(msg.sender == manager);
+
+        address winner = players[random() % players.length];
+        players = new address[](0);
+
+        (bool success,) = winner.call{value: address(this).balance}("");
+        require(success);
+    }
+
+}
