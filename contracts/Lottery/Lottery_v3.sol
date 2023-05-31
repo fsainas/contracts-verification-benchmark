dcf9141eae7b756fc213a860c9310cc9169697ed
Franco Sainas <franco@sainas.me>
2023-05-31 16:58:56 +0200

    Lottery: added v3

---
+++ contracts/Lottery/Lottery_v3.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.18;
+
+contract Lottery {
+    address[] private players;
+
+    function enter() external payable {
+        require(msg.value == .01 ether);
+        
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encode(block.prevrandao)));
+    }
+
+    function pickWinner() public {
+        address winner = players[random() % players.length];
+        players = new address[](0);
+
+        uint fee = address(this).balance / 100;
+
+        (bool success,) = msg.sender.call{value: fee}("");
+        require(success);
+
+        (success,) = winner.call{value: address(this).balance}("");
+        require(success);
+    }
+
+}
