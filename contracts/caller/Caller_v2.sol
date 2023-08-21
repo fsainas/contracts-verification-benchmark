24080c97db12c2e43cfd9dd889e9806be09b0927
Daniele Pusceddu <43929743+danielepusceddu@users.noreply.github.com>
2023-08-21 11:24:10 +0200

    Initial readme, ground truths and sources for Caller

---
+++ contracts/caller/Caller_v2.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity >= 0.8.2;
+import "./lib/ReentrancyGuard.sol";
+
+contract Caller is ReentrancyGuard {
+    uint data;
+
+    function callyourself() public nonReentrant {
+        msg.sender.call("");
+    }
+
+}
---
+++ contracts/caller/Caller_v3.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity >= 0.8.2;
+
+contract Caller {
+    uint data;
+
+    function callyourself() public {
+        msg.sender.call("");
+    }
+
+    function modifystorage(uint newdata) public {
+        data = newdata;
+    }
+
+}
