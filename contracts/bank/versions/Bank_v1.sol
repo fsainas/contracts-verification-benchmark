633fa6a7146bcaeed8e9a5bbaa336fc8eea266b2
giuliamatri <127243730+giuliamatri@users.noreply.github.com>
2023-11-27 19:52:39 +0100

    Add files via upload

---
+++ contracts/bank/versions/Bank_v1.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >= 0.8.2;
+
+
+/// @custom:version conformant to specification
+contract Bank {
+    mapping (address => uint) balances;
+
+    receive() external payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint amount) public {
+        require(amount > 0);
+        require(amount <= balances[msg.sender]);
+
+        balances[msg.sender] -= amount;
+
+        (bool success,) = msg.sender.call{value: amount}("");
+        require(success);
+    }
+
+}
---
+++ contracts/bank/versions/Bank_v3.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >= 0.8.2;
+
+
+/// @custom:version no `amount <= balances[msg.sender]` check and `balances[msg.sender]` is decremented by `amount - 1` in `withdraw()`
+contract Bank {
+    mapping (address => uint) balances;
+
+    receive() external payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint amount) public {
+        require(amount > 0);
+        //require(amount <= balances[msg.sender]);
+
+        balances[msg.sender] -= amount - 1;
+
+        (bool success,) = msg.sender.call{value: amount}("");
+        require(success);
+    }
+
+}
