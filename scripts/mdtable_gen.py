18ced02c7d27c43efd9557d28bf6951feaaa9294
Franco Sainas <franco@sainas.me>
2023-12-29 09:40:42 +0100

    add footnote support

--- scripts/mdtable_gen.py
+++ scripts/mdtable_gen.py
@@ -13,7 +13,7 @@ import csv
 import sys
 
 
-MIN_CELL_WIDTH = 5
+MIN_CELL_WIDTH = 7
 MARGIN = 2   # between cells
 
 
@@ -22,22 +22,22 @@ def gen_from_dict(data):
     Writes the markdown table as a list of rows.
 
     Args:
-        data (dict): { (col, row): data }
+        data (dict): { (col, row): (data, footnote) }
 
     Returns:
         str: markdown table
     """
 
     key_f = lambda x: int(x[1:])     # e.g. p1 -> 1
-    cols = sorted(set([c for c, _ in data.keys()]), key=key_f)
-    rows = sorted(set([r for _, r in data.keys()]), key=key_f)
+    cols_ids = sorted(set([c for c, _ in data.keys()]), key=key_f)
+    rows_ids = sorted(set([r for _, r in data.keys()]), key=key_f)
 
-    if not rows:
+    if not rows_ids:
         print("\nEmpty cm.csv file!\n", file=sys.stderr)
         sys.exit(1)
 
-    max_c_length = max([len(c) for c in cols])
-    max_r_length = max([len(r) for r in rows])
+    max_c_length = max([len(c) for c in cols_ids])
+    max_r_length = max([len(r) for r in rows_ids])
 
     cell_width = max(
             MIN_CELL_WIDTH,
@@ -47,33 +47,41 @@ def gen_from_dict(data):
 
     # Create the header row
     header = (
-            "|" + " " * (max_r_length + 6) + "| " +
-            "| ".join([c + " "*(cell_width-len(c)-1) for c in cols]) +
-            "|"
+            '|' + ' ' * (max_r_length + 6) + '| ' +
+            '| '.join([c + ' '*(cell_width-len(c)-1) for c in cols_ids]) +
+            '|'
     )
     separator = (
-            "|" + "-" * (max_r_length + 6) + "|" +
-            "|".join([
-                "-" * len(header.split('|')[i])
-                for i in range(2, len(cols) + 2)
-            ]) + "|"
+            '|' + '-' * (max_r_length + 6) + '|' +
+            '|'.join([
+                '-' * len(header.split('|')[i])
+                for i in range(2, len(cols_ids) + 2)
+            ]) + '|'
     )
 
     # Create the rows
     table_rows = []
-    for r in rows:
-        row_str = "| **" + r + "** |"
-        for c in cols:
-            d = data[(c, r)]
-            if d is not None:
-                row_str += " " + d + " " * (cell_width-len(d)-1) + "|"
-            else:
-                row_str += cell_width
+    fn_rows = []        # footnotes to put at the end of the table
+    fn_counter = 1      # next free footnote index
+    for r in rows_ids:
+        row = '| **' + r + '** |'   # row index in bold
+        for c in cols_ids:
+            d, fn = data[(c, r)]
+            if d:   # if there is data add it to the table
+                if len(fn) > 0:
+                    fn_mark = '[^' + str(fn_counter) + ']'        # e.g. [^1]
+                    row += ' ' + d + fn_mark + ' ' * (cell_width-len(d+fn_mark)-1) + '|'
+                    fn_counter += 1
+                    fn_rows.append(fn_mark + ': ' + fn)
+                else:
+                    row += ' ' + d + ' ' * (cell_width-len(d)-1) + '|'
+            else:   # else preserve spaces
+                row += cell_width
 
-        table_rows.append(row_str)
+        table_rows.append(row)
 
     # Combine everything into a Markdown table
-    mdtable = "\n".join([header, separator] + table_rows) + "\n"
+    mdtable = '\n'.join([header, separator] + table_rows + [' '] + fn_rows) + '\n'
 
     return mdtable
 
@@ -87,13 +95,15 @@ def gen_from_csv(input_file):
 
         next(csv_reader)
 
-        data = {}      # (c,r): data
+        data = {}      # (c,r): (data, footnote)
 
         for row in csv_reader:
             if row[0] == '#':
                 continue;
-            c, r, d = row[0], row[1], row[2]
-            data.update({(c, r): d})
+            c, r, d= row[0], row[1], row[2]
+            fn = row[3] if len(row) == 4 else ''    # footnote
+
+            data.update({(c, r): (d, fn)})
 
         mdtable = gen_from_dict(data)
 
