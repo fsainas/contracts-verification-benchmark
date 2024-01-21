4810250545b2797cc72c377ab5026e7638833396
Franco Sainas <franco@sainas.me>
2024-01-21 19:45:19 +0100

    merged runc.py and runs.py in run_certora.py and run_solcmc.py

--- scripts/run_certora.py
+++ scripts/run_certora.py
@@ -1,9 +1,9 @@
-"""
+'''
 Operates on either a single file or every file within a directory.
 
 Usage:
     python run_certora.py -c <file_or_dir> -s <spec_file> -o <output_dir>
-"""
+'''
 
 from string import Template
 from multiprocessing import Pool
@@ -48,33 +48,19 @@ def has_critical_error(output):
     return re.search(pattern, output, re.DOTALL)
 
 
-def get_properties(spec_path):
-    """
-    Retrieves the list of properties defined in CVL spec files.
-
-    Returns:
-        list: The list of property names.
-    """
-    return (
-            glob.glob(f'{spec_path}/*.spec')
-            if os.path.isdir(spec_path)
-            else [spec_path]
-    )
-
-
 def run(contract_path, spec_path):
-    """
+    '''
     Runs a single certora experiment.
 
     Returns:
         tuple: (outcome, log)
-    """
+    '''
 
     # Check if the contract to verify exists
     if not os.path.isfile(contract_path):
         msg = f'{contract_path} not found.'
         logging.error(msg)
-        return (ERROR, msg)
+        return ERROR, msg
 
     # Name of the Solidity contract to verify
     contract_name = utils.get_contract_name(contract_path)
@@ -83,7 +69,7 @@ def run(contract_path, spec_path):
     if not contract_name:
         msg = f'Could not retrieve {contract_path} contract name.'
         logging.error(msg)
-        return (ERROR, msg)
+        return ERROR, msg
 
     # Process specs file
     negate = False
@@ -100,20 +86,20 @@ def run(contract_path, spec_path):
         if not has_assert and not has_satisfy:
             msg = f'Error in {spec_path}: No "assert" or "satisfy" found.'
             logging.error(msg)
-            return (ERROR, msg)
+            return ERROR, msg
 
         # Exit if both are used
         if has_assert and has_satisfy:
-            error = f'Error in {spec_path}: Combining both "satisfy" and "assert" is not allowed.'
-            logging.error(error)
-            return (ERROR, error)
+            msg = f'Error in {spec_path}: Combining both "satisfy" and "assert" is not allowed.'
+            logging.error(msg)
+            return ERROR, msg
 
         # Process tags
         # Tag Nondefinable
         nondef = re.search('/// @custom:nondef (.*)', spec_code)
         if nondef:
             print(f'{contract_path}: {NONDEFINABLE} (nondefinable)')
-            return (NONDEFINABLE, nondef.group(1))
+            return NONDEFINABLE, nondef.group(1)
 
         # Tag Negate
         negate = re.search('/// @custom:negate', spec_code)
@@ -145,7 +131,7 @@ def run(contract_path, spec_path):
 
     if has_critical_error(log.stdout):
         logging.error(log.stdout)
-        return (ERR, log.stdout)
+        return ERR, log.stdout
 
     # Save result
     is_positive = no_errors_found(log.stdout)
@@ -157,88 +143,89 @@ def run(contract_path, spec_path):
         res = WEAK_NEGATIVE if has_assert else STRONG_NEGATIVE
 
     print(f'{contract_path}, {spec_path}: {res}')
-    return (res, f'{log.stdout}\n\n{log.stderr}')
+    return res, f'{log.stdout}\n\n{log.stderr}'
 
 
-def run_log(id, contract_path, spec_path, logs_dir):
-    """
+def run_log(id, contract_path, spec_path, logs_dir=None):
+    '''
     Calls run() and writes the log.
-    """
-    (outcome, log) = run(contract_path, spec_path)
-    utils.write_log(logs_dir + id + '.log', log)
-    return (id, outcome)
+    '''
+    outcome, log = run(contract_path, spec_path)
+    if logs_dir:
+        utils.write_log(logs_dir + id + '.log', log)
+    else:
+        print(log)
+        print(f'Result: {outcome}') 
+    return id, outcome
 
 
-def run_all(contracts_dir, spec_path, logs_dir):
-    """
+def run_all(contracts_paths, specs_paths, logs_dir=None):
+    '''
     Runs certora on all files of a directory.
 
     Args:
-        contracts_dir (str): Contracts directory path.
-        spec_path (str): CVL spec dir path.
+        contracts_paths (list): Contracts paths.
+        specs_paths (list): CVL specs paths.
         log_dir (str): Log directory path.
 
     Returns:
         dict: {key_v*: outcome}
-    """
+    '''
     outcomes = {}
-
-    specs = get_properties(spec_path)   # list of paths
 
     # Specific properties
     bound_properties_paths = list(filter(
             lambda x: re.search(".*_v.*", x),
-            specs))
+            specs_paths))
     unbound_properties_paths = list(
-            set(specs) - set(bound_properties_paths))
+            set(specs_paths) - set(bound_properties_paths))
 
-    inputs = []     # inputs for run_certora_parallel()
+    inputs = []     # inputs for run_log()
 
-    for v_path in os.listdir(contracts_dir):
-        if not os.path.isdir(contracts_dir + v_path):     # lib/ is ignored
+    for v_path in contracts_paths:
+        # Extract base id from base path (e.g. v1)
+        v_id = v_path.split('/')[-1].split('_')[-1].split('.')[0]
 
-            # Extract base id from base path (e.g. v1)
-            v_id = v_path.split('/')[-1].split('_')[-1].split('.')[0]
+        v_bound = list(filter(
+                lambda x: re.search(f'.*_{v_id}.*', x),
+                bound_properties_paths))
 
-            v_bound = list(filter(
-                    lambda x: re.search(f'.*_{v_id}.*', x),
-                    bound_properties_paths))
+        v_unbound = unbound_properties_paths
 
-            v_unbound = unbound_properties_paths
+        for bp_path in v_bound:
+            p_id = bp_path.split('/')[-1].split('_')[0]     # ../p1_v1.sol -> p1
+            # Remove bound properties from the unbound variants
+            v_unbound = list(filter(
+                    lambda x: not re.search(f'{p_id}', x),
+                    v_unbound
+                    ))
 
-            for bp_path in v_bound:
-                p_id = bp_path.split('/')[-1].split('_')[0]     # ../p1_v1.sol -> p1
-                # Remove bound properties from the unbound variants
-                v_unbound = list(filter(
-                        lambda x: not re.search(f'{p_id}', x),
-                        v_unbound
-                        ))
+        # List of properties to test for the current version
+        v_properties_paths = v_bound + v_unbound
 
-            v_properties_paths = v_bound + v_unbound
-
-            for s_path in v_properties_paths:
-                id = (  # e.g. p1_v1
-                        s_path.split('.')[-2].split('/')[-1].split('_')[0] +
-                        '_' +
-                        v_path.split('_')[-1].split('.sol')[0]
-                )
-                inputs.append((id, contracts_dir + v_path, s_path, logs_dir))
+        for s_path in v_properties_paths:
+            id = (  # e.g. p1_v1
+                    s_path.split('.')[-2].split('/')[-1].split('_')[0] +
+                    '_' +
+                    v_path.split('_')[-1].split('.sol')[0]
+            )
+            inputs.append((id, v_path, s_path, logs_dir))
 
     with Pool(processes=THREADS) as pool:
         # [(id, outcome), ...]
         results = pool.starmap(run_log, inputs)
-        for (id, outcome) in results:
+        for id, outcome in results:
             outcomes[id] = outcome
 
     return outcomes
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
             '--contracts',
             '-c',
-            help='Contracts directory',
+            help='Contracts dir or contract file.',
             required=True
             )
     parser.add_argument(
@@ -251,26 +238,50 @@ if __name__ == "__main__":
             '--output',
             '-o',
             help='Output directory.',
-            required=True
+            #required=True
     )
     args = parser.parse_args()
 
-    contracts_dir = (
+    # Remove final slash
+    contracts = (
             args.contracts
-            if args.contracts[-1] == '/'
-            else args.contracts + '/'
+            if args.contracts[-1] != '/'
+            else args.contracts[:-1]
             )
-    output_dir = args.output if args.output[-1] == '/' else args.output + '/'
-    logs_dir = output_dir + 'logs/'
-    spec_path = args.specs if args.specs[-1] == '/' else args.specs + '/'
 
-    outcomes = run_all(contracts_dir, spec_path, logs_dir)
+    # Get contracts paths
+    contracts_paths = (
+            glob.glob(f'{contracts}/*.sol')
+            if os.path.isdir(contracts)
+            else [contracts]
+    )
 
-    out_csv = [utils.OUT_HEADER]
+    # Remove final slash
+    specs = (
+            args.specs
+            if args.specs[-1] != '/'
+            else args.specs[:-1]
+            )
 
-    for id in outcomes.keys():
-        p = id.split('_')[0]
-        v = id.split('_')[1]
-        out_csv.append([p, v, outcomes[id]])
+    # Get specs paths
+    specs_paths = (
+            glob.glob(f'{specs}/*.spec')
+            if os.path.isdir(specs)
+            else [specs]
+    )
 
-    utils.write_csv(output_dir + 'out.csv', out_csv)
+    if args.output:
+        output_dir = args.output if args.output[-1] == '/' else args.output + '/'
+        logs_dir = output_dir + 'logs/'
+
+        outcomes = run_all(contracts_paths, specs_paths, logs_dir)
+
+        out_csv = [utils.OUT_HEADER]
+        for id in outcomes.keys():
+            p = id.split('_')[0]
+            v = id.split('_')[1]
+            out_csv.append([p, v, outcomes[id]])
+
+        utils.write_csv(output_dir + 'out.csv', out_csv)
+    else:
+        run_all(contracts_paths, specs_paths)
