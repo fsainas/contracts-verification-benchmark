d008babec5523420e5db12df1b3dd88e3aeb3ff9
Franco Sainas <franco@sainas.me>
2024-01-29 21:54:23 +0100

    refactor: cleaned certora scripts

---
+++ scripts/inject_getters.py
@@ -0,0 +1,65 @@
+'''
+Script for injecting getters into contracts to be verified with Certora.
+'''
+from setup.injector import inject_before_last_bracket
+from pathlib import Path
+import argparse
+import glob
+import os
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            '--contracts',
+            '-c',
+            help='Contract file or dir path.',
+            required=True
+    )
+    parser.add_argument(
+            '--getters',
+            '-g',
+            help='Getters file path.',
+            required=True
+    )
+    parser.add_argument(
+            '--output',
+            '-o',
+            help='Output directory path.',
+    )
+    args = parser.parse_args()
+
+    args.output = args.output if args.output[-1] == '/' else args.output + '/'
+
+    args.contracts = (
+            args.contracts
+            if args.contracts[-1] != '/'
+            else args.contracts[:-1]
+            )
+
+    contracts_paths = (
+            glob.glob(f'{args.contracts}/*v*.sol')
+            if os.path.isdir(args.contracts)
+            else [args.contracts]
+    )
+
+    for contract_path in contracts_paths:
+        # Save contract lines of code
+        contract = []
+        with open(contract_path, 'r') as f:
+            contract = f.readlines()
+
+        getters = []
+        with open(args.getters, 'r') as f:
+            getters = f.readlines()
+
+        contract_with_getters = ''.join(l for l in inject_before_last_bracket(contract, getters))
+
+        # Extract file name from contract path
+        filename = Path(contract_path).name
+
+        if args.output:
+            with open(args.output + filename, 'w') as f:
+                f.write(contract_with_getters)
+        else:
+            print(contract_with_getters)
